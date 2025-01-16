a09391a03c1104258fbca29adb39345277eab03b
d3m3sh <jmoucadel@outlook.com>
2025-01-16 23:23:23 +0100

    ano

--- lib/data/contact.ts
+++ lib/data/contact.ts
@@ -3,6 +3,6 @@
 export const contact = {
   github: "github.com/x1904",
   githubBis: "github.com/mo0k",
-  linkedin: "linkedin.com/in/julien-moucadel-dev/",
+  linkedin: "linkedin.com/in/xxx/",
   email: "jmoucadel@outlook.com"
 };
